d1886f7e9ce1892fb6405a26ec98476e5885102b
Dmitriy <dmitriy.moiseev.86@gmail.com>
2021-08-10 04:04:52 +0300

    lesson summary 4.0

--- lesson_python2.py
+++ lesson_python2.py
@@ -60,28 +60,28 @@
 #         result_km = result_km + a
 # print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
 
-# Задание 7
-goods = []
-features = {'name': '', 'price': '', 'quantity': '', 'unit': ''}
-analytics = {'name': [], 'price': [], 'quantity': [], 'unit': []}
-num = 0
-feature_ = None
-control = None
-while True:
-    control = input("For quit press 'Q', for continue press 'Enter', for analytics press 'A'").upper()
-    if control == 'Q':
-        break
-    num += 1
-    if control == 'A':
-        print(f'\n Current analytics \n {"-" * 30}')
-        for key, value in analytics.items():
-            print(f'{key[:25]:>30}: {value}')
-            print("-" * 30)
-    for f in features.keys():
-        feature_ = input(f'Input feature "{f}"')
-        features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
-        analytics[f].append(features[f])
-    goods.append((num, features))
+# # Задание 7
+# goods = []
+# features = {'name': '', 'price': '', 'quantity': '', 'unit': ''}
+# analytics = {'name': [], 'price': [], 'quantity': [], 'unit': []}
+# num = 0
+# feature_ = None
+# control = None
+# while True:
+#     control = input("For quit press 'Q', for continue press 'Enter', for analytics press 'A'").upper()
+#     if control == 'Q':
+#         break
+#     num += 1
+#     if control == 'A':
+#         print(f'\n Current analytics \n {"-" * 30}')
+#         for key, value in analytics.items():
+#             print(f'{key[:25]:>30}: {value}')
+#             print("-" * 30)
+#     for f in features.keys():
+#         feature_ = input(f'Input feature "{f}"')
+#         features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
+#         analytics[f].append(features[f])
+#     goods.append((num, features))
 
 
 
